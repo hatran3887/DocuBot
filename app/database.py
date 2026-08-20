9931d15edcb1264c035ab610a36a6949760b50cb
Ha Tran <hatran3887@googlemail.com>
2026-08-20 15:41:44 +0200

    Add engine, session factory for db

---
+++ app/database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from collections.abc import Generator
+from app.config import settings
+
+
+engine = create_engine(
+    str(settings.database_url),
+    pool_pre_ping=True,
+    echo=settings.debug
+)
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
